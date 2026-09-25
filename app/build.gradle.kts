fc0b006a2f30eb45a0d352227aef79ccdbf35400
braze-taffo <2524129681@qq.com>
2026-09-25 13:50:43 +0800

    版本提升至 v1.7.0（versionCode 37）

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "app.tellev"
         minSdk = 31
         targetSdk = 36
-        versionCode = 36
-        versionName = "1.6.6.1"
+        versionCode = 37
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
